58e7d2027aa787ef05db1b8eaaa453bf458301c2
Nadine Mansour <nadine.mansour94@gmail.com>
2016-11-08 22:02:48 +0200

    first commit

---
+++ Assets/Scripts/AJController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AJController : MonoBehaviour {
+	private Animator animator;
+	private float speed = 3.0f;
+	private Rigidbody rb;
+	private bool walkForward, suspendMove;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		walkForward = false;
+
+		if (Input.GetKey("w") && !suspendMove){
+			//Walk forward
+			walkForward = true;
+			transform.Translate (0, 0, Input.GetAxis("Vertical")*2*Time.deltaTime);
+		}
+		
+
+		if (Input.GetKey("d")){
+			//rotate clockwise
+			transform.Rotate(Vector3.up * 50 * Time.deltaTime);
+			walkForward = true;
+		}
+		
+
+		if (Input.GetKey("a")){
+			//rotate anti-clockwise
+			transform.Rotate(Vector3.up * -50 * Time.deltaTime);
+			walkForward = true;
+		}
+		
+
+		animator.SetBool("walkForward", walkForward );
+	}
+
+	void OnCollisionEnter (Collision col){
+		if (col.gameObject.CompareTag("wall") || col.gameObject.CompareTag("furniture")){
+			suspendMove = true;
+		}
+	}
+
+	void OnCollisionExit (Collision col){
+		if (col.gameObject.CompareTag("wall") || col.gameObject.CompareTag("furniture")){
+			suspendMove = false;
+		}
+	}
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 positionOffset;
+	private Vector3 rotationOffset;
+	// Use this for initialization
+	void Start () {
+		positionOffset = player.transform.position - transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w")){
+			transform.position =  new Vector3(player.transform.position.x-positionOffset.x,transform.position.y,player.transform.position.z-positionOffset.z);
+		}
+		if(Input.GetKey("a")||Input.GetKey("d")){
+			
+		}
+	}
+}
